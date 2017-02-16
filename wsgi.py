45007cce8cd8fa683b54c474dcb7c8029672c57c
8cbx <8cbx@users.noreply.github.com>
2017-02-16 02:57:08 -0600

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -16,7 +16,7 @@ __Author__ = "cdhigh <https://github.com/cdhigh>"
 from wsgiref.util import is_hop_by_hop
 import os, urllib, urllib2, socket, bottle
 
-ALLOW_KEYS = 'xzSlE'
+ALLOW_KEYS = '1871c15d9dc1e2c7'
 
 application = app = bottle.Bottle()
 
