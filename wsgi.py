0df02f678ba3c0679ec655f01e5f85c7f6791a15
8cbx <8cbx@users.noreply.github.com>
2017-02-18 00:02:59 -0600

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -33,6 +33,7 @@ def Home():
     
     if url and k:
         url = urllib.unquote(url.encode('utf-8')).replace(' ', r'%20')
+        print url
         try:
             req = urllib2.Request(url)
             req.add_header('User-Agent', "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0)")
